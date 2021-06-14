b1e48463482a333c5e0b2b406a3d6f10f73ad560
Lesly Zerna <lesly.zerna.bo07@gmail.com>
2021-06-14 19:48:41 -0400

    Update hellopython.py
    update

--- hellopython.py
+++ hellopython.py
@@ -1,8 +1,14 @@
 # hello world
 # ref: https://docs.python-guide.org/writing/style/
 
-if attr == True:
-    print('True!')
-    
-if attr == None:
+# Just check the value
+if attr:
+    print('attr is truthy!')
+
+# or check for the opposite
+if not attr:
+    print('attr is falsey!')
+
+# or, since None is considered false, explicitly check for it
+if attr is None:
     print('attr is None!')
