e63876feefb8c77688bdd22a9640fcf210adb73a
Lesly Zerna <lesly.zerna.bo07@gmail.com>
2021-06-21 11:05:25 -0400

    Create var_4_loops.py
    examples to check

---
+++ var_4_loops.py
@@ -0,0 +1,11 @@
+# examples https://code.tutsplus.com/tutorials/top-15-best-practices-for-writing-super-readable-code--net-8118
+
+// $i for loop counters
+for ($i = 0; $i < 100; $i++) {
+ 
+    // $j for the nested loop counters
+    for ($j = 0; $j < 100; $j++) {
+ 
+    }
+}
+
